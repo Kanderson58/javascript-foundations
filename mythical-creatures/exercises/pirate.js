c57733e955fe49e16718b1d2c22a0795b23fa253
Kara Anderson <kanderson8585@gmail.com>
2022-12-09 09:41:17 -0700

    Pass all pirate tests

---
+++ mythical-creatures/exercises/pirate.js
@@ -0,0 +1,29 @@
+class Pirate {
+    constructor(pirateName, pirateJob = "scallywag") {
+        this.name = pirateName
+        this.job = pirateJob
+        this.cursed = false
+        this.booty = 0
+    }
+    robShip() {
+        if (this.booty < 500 && !this.cursed) {
+            this.booty += 100
+            return "YAARRR!"
+        } else {
+            this.cursed = true
+            return "ARG! I\'ve been cursed!"
+        }
+    }
+    liftCurse() {
+        if (!this.cursed) {
+            return "You don\'t need to lift a curse!"
+        } else {
+            this.booty = 200
+            this.cursed = false
+            return "Your curse has been lifted!"
+        }
+    }
+}
+
+
+module.exports = Pirate
