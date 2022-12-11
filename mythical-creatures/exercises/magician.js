22d9c64e802b6502d7b959b09b6bc3008b5a90bd
Kara Anderson <kanderson8585@gmail.com>
2022-12-10 17:50:17 -0700

    Complete magician tests

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -3,6 +3,25 @@ class Magician {
         this.name = `The Great ${magicianName.name}`
         this.assistant = magicianName.assistant
         this.favoriteAccessory = magicianName.clothing || favoriteAccessory
+        this.confidencePercentage = 10
+    }
+    performIncantation(incantation) {
+        return `${incantation.toUpperCase()}!`
+    }
+    performTrick() {
+        this.confidencePercentage += 10
+        if (this.favoriteAccessory === "top hat") {
+            return "PULL RABBIT FROM TOP HAT"
+        } else {
+            return "PULL DOVE FROM SLEEVE"
+        }
+    }
+    performShowStopper() {
+        if (this.confidencePercentage > 100 && this.assistant === true) {
+            return "WOW! The magician totally just sawed that person in half!"
+        } else {
+            return "Oh no, this trick is not ready!"
+        }
     }
 
 }
