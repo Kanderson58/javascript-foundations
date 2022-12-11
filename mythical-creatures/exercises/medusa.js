81992f5341216b8b0f989e6d3ef6e7c029b00a7f
Kara Anderson <kanderson8585@gmail.com>
2022-12-10 19:07:59 -0700

    Complete medusa tests

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,23 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+    constructor(medusaName) {
+        this.name = medusaName
+        this.statues = []
+    }
+    gazeAtVictim(victim) {
+        var newVictim = new Statue(victim.name) 
+        if (this.statues.length < 3) {
+            this.statues.push(newVictim)
+        } else {
+            var oldVictim = new Person(this.statues[0].name, "relieved")
+            this.statues.shift()
+            this.statues.push(newVictim)
+            return oldVictim
+        }
+    }
+}
+
+
+module.exports = Medusa
---
+++ mythical-creatures/exercises/person.js
@@ -0,0 +1,8 @@
+class Person {
+    constructor(personName, personMood = "frightened") {
+        this.name = personName
+        this.mood = personMood
+    }
+}
+
+module.exports = Person
