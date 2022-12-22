baaff3808a7b578b96fbdb5756dc840c7e2e2bec
Kara Anderson <kanderson8585@gmail.com>
2022-12-22 08:20:05 -0700

    Complete road race tests

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,22 @@
 class Roadrace {
-
+    constructor(raceObject) {
+        this.name = raceObject.title;
+        this.location = raceObject.city;
+        this.distance;
+        this.participants = [];
+    }
+    setDistance(numMiles) {
+        this.distance = numMiles;
+        return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`;
+    }
+    registerParticipants(participantName) {
+        this.participants.push(participantName);
+    }
+    completeRace() {
+        for(var i = 0; i < this.participants.length; i++) {
+            this.participants[i].runRace(this.name, this.distance)
+        }
+    }
 }
 
 module.exports = Roadrace;
--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,22 @@
 class Runner {
-
+    constructor(runnerName, runnerAge) {
+        this.name = runnerName;
+        this.age = runnerAge;
+        this.milesRun = 0;
+        this.fitness = 25;
+        this.completedRaces = [];
+    }
+    runSomeMiles(numMiles) {
+        this.milesRun += numMiles;
+        this.fitness += numMiles;
+    }
+    stretch() {
+        this.fitness += .5;
+    }
+    runRace(raceName, raceMiles) {
+        this.runSomeMiles(raceMiles);
+        this.completedRaces.push(raceName);
+    }
 }
 
 module.exports = Runner;
