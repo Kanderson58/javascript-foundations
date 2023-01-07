d549aa283b174a552cbccc1f26770a8addf29696
Kara Anderson <kanderson8585@gmail.com>
2023-01-07 12:19:54 -0700

    Pass all ski lift tests

--- ski-lift/src/Lift.js
+++ ski-lift/src/Lift.js
@@ -1,7 +1,32 @@
 var Skier = require('./Skier');
 
 class Lift {
-  
+    constructor(maxPeople) {
+        this.inService = true;
+        this.limit = maxPeople;
+        this.skiers = [];
+        this.safetyBar = "up";
+    }
+    admitSkier(name, hasTicket) {
+        var skier = new Skier(name, hasTicket)
+        if(this.skiers.length < this.limit && hasTicket) {
+            this.skiers.push(skier);
+        } else if(this.skiers.length >= this.limit){
+            return `Sorry, ${skier.name}. Please wait for the next lift!`;
+        } else {
+            return `Sorry, ${skier.name}. You need a lift ticket!`;
+        }
+    }
+    startLift() {
+        var remainingSpots = this.limit - this.skiers.length;
+        if(this.skiers.length === this.limit) {
+            this.safetyBar = "down";
+        } else if (remainingSpots === 1){
+            return 'We still need 1 more skier!'
+        } else {
+            return `We still need ${remainingSpots} more skiers!`
+        }
+    }
 }
 
 module.exports = Lift;
